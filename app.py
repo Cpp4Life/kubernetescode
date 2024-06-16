517c96bff251de2ebbaf7c01ec9e54272a2eb75b
Cpp4Life <62204436+Cpp4Life@users.noreply.github.com>
2024-06-16 23:15:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Now, I can create my first CI/CD Pipeline using Jenkins and ArgoCD! Version 1.0'
+    return 'Now, I can create my first CI/CD Pipeline using Jenkins and ArgoCD! Version 2.0 - Fix Connection Issue'
